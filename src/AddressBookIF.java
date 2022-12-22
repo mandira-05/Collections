f981aacee833caf2ea99aa92b6eab22a616a69c9
mandira-05 <mandira.sn.gowda@gmail.com>
2022-12-22 18:54:41 +0530

    Used java stream to check no duplicate conatct is entered

---
+++ src/AddressBookIF.java
@@ -0,0 +1,8 @@
+public interface AddressBookIF {
+
+    public void operation();
+    public void addContact();
+    public void displayContents();
+    public void editPerson();
+    public void deletePerson();
+}
---
+++ src/AddressBookMain.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class AddressBookMain {
+
+    public static Scanner scannerObject = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        System.out.println("---------- Welcome To Address Book Pr1ogram!! ----------");
+        AddressBookDirectory addressBookDirectory = new AddressBookDirectory();
+        addressBookDirectory.operationDirectory();
+    }
+
+}
